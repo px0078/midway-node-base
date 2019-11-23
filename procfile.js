5a5df1bbd49ba7135f23d3c354fa7c49810a9544
xu peng <908228507@qq.com>
2019-11-24 00:43:10 +0800

    feat: 增加了procfile.js文件 

--- procfile.js
+++ procfile.js
@@ -1,19 +1,23 @@
+'use strict';
+
 module.exports = (pandora) => {
 
   pandora
-  
-    // 默认启动到 worker 进程分组
-    .cluster('./app.js'); 
- 
-  /* 自定义 Worker 数量
-  pandora
-    .process('worker')
-    
-    // 修改 worker 进程分组启动 2 个 worker 
-    // ，默认是 pandora.dev ? 1 : 'auto'。
-    // 意思就是 pandora dev 启动的话就不 Cluster 
-    // ，如果 pandora start 启动的话就 Cluster 到 CPU 数量。
-    .scale(2); 
-  */
-    
-}
+
+    // 定义一个进程，名字叫 processA
+    .process('py_edu')
+	
+    // 如果 scale 大于 1 ，将使用 Node.js 的 Cluster 模块自动产生进程组
+    // 默认值即是 1
+    .scale(1)
+	
+    // 定义进程环境变量，创建出来的进程中可以通过 process.env 获得
+    .env({
+      ENV_VAR1: 'VALUE_OF_ENV_VAR1'
+    })
+	
+	// 这个进程的入口文件地址
+	.entry(require.resolve('midway/server'));
+
+
+};
