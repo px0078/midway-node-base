b55b13db05f4b70a04e0209c0d28c9fb514fa4fa
xu peng <908228507@qq.com>
2019-12-15 19:41:31 +0800

    feat: 增加 format paging interface

--- src/app/extend/response.ts
+++ src/app/extend/response.ts
@@ -1,7 +1,13 @@
 
+import { IPagingRes } from '@/interface/response';
+
+interface IPagingReulst extends IPagingRes {
+  list: [any]
+}
+
 export default {
   format: {
-    paging({ results, total, pageSize, pageNumber }: any) {
+    paging({ results, total, pageSize, pageNumber }: any): IPagingReulst {
       return {
         list: results,
         pageSize: Number(pageSize),
