30d3516b584faa53f103b44f956b88c6a461f1bd
xu peng <908228507@qq.com>
2020-01-02 22:18:06 +0800

    refactor: 修改鉴权中间件

--- src/app/middleware/authorize.ts
+++ src/app/middleware/authorize.ts
@@ -46,43 +46,48 @@ export class authMiddleware implements WebMiddleware {
       }
   
       // 根据用户的 id 来查找用户所在组
-      const groupsList = await ctx.model.AuthGroup.find({
-        users: userInfo.id,
+      let groupModulesList = await ctx.model.AuthGroup.find(
+        {
+          users: userInfo.id,
+        },
+        {
+          modules: 1,
+        }
+      );
+      
+      if (!groupModulesList) return noAccess()
+      groupModulesList = flatten(groupModulesList)
+  
+      if (!groupModulesList.length) return noAccess();
+
+      // 遍历用户的所有组
+      // for (let i = 0, l = groupsList.length; i < l; i++) {
+      //   const buttonGroup = groupsList[i].modules || [];
+  
+      //   // 如果用户所在组没有权限就返回
+      //   if (buttonGroup.length === 0) return noAccess();
+  
+      // 查找用户所在组里所有的 uri
+      const result = await ctx.model.AuthModule.find({ _id: { $in: groupModulesList } });
+
+      // 查不到就返回无权限
+      if (result === null || !result.length) return noAccess();
+  
+      let uri = result.map((item: any) => {
+        if (item.uri && item.uri.trim()) {
+          return item.uri.split(',');
+        }
+        return '';
       });
   
-      if (groupsList === null || !groupsList.length) {
-        noAccess();
+      uri = uniq(compact(flatten(uri)));
+
+      // 找到了之后看该用户的用户组是否有此权限的id
+      if (uri.includes(action)) {
+        await next();
+        return true;
       }
-  
-      // 遍历用户的所有组
-      for (let i = 0, l = groupsList.length; i < l; i++) {
-        const buttonGroup = groupsList[i].modules || [];
-  
-        // 如果用户所在组没有权限就返回
-        if (buttonGroup.length === 0) return noAccess();
-  
-        // 查找用户所在组里所有的 uri
-        const result = await ctx.model.AuthModule.find({ _id: { $in: buttonGroup } });
-  
-        // 查不到就返回无权限
-        if (result === null || !result.length) return noAccess();
-  
-        let uri = result.map((item: any) => {
-          if (item.uri && item.uri.trim()) {
-            return item.uri.split(',');
-          }
-          return '';
-        });
-  
-        uri = uniq(compact(flatten(uri)));
-  
-        // 找到了之后看该用户的用户组是否有此权限的id
-        if (uri.includes(action)) {
-          await next();
-  
-          return true;
-        }
-      }
+      // }
       noAccess();
     }
   }
